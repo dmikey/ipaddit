d62784805347e93b35a4d3f1ad27f78e5c862546
Derek Anderson <dmikey@users.noreply.github.com>
2026-08-01 13:00:42 -0500

    Initial Commit

---
+++ GitCodeStudio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitCodeStudio
+//
+//  Created by Derek Anderson on 8/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitCodeStudio/GitCodeStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCodeStudioApp.swift
+//  GitCodeStudio
+//
+//  Created by Derek Anderson on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GitCodeStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
